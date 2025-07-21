0ce53d48073f43d9e9baffb5edd811a25d358840
KollaRajesh0450 <2000030838cse@gmail.com>
2025-07-21 14:46:07 +0530

    Initial commit - Wipro training codes day-wise

---
+++ day2/luckyday/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Luckyday
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter your Lucky day :");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Moday is your lucky day");
+                break;
+            case 2:
+                Console.WriteLine("tuesday is your lucky day");
+                break;
+            case 3:
+                Console.WriteLine("wednesday is your lucky day");
+                break;
+            case 4:
+                Console.WriteLine("thurday is your lucky day");
+                break;
+            case 5:
+                Console.WriteLine("friday is your lucky day");
+                break;
+            case 6:
+                Console.WriteLine("saturday is your lucky day");
+                break;
+            case 7:
+                Console.WriteLine("Sunday is your lucky day");
+                break;
+            default:
+                Console.WriteLine("All days are lucky");
+                break;
+
+        }
+    }
+}
---
+++ day2/studentdetails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class StudentDetails
+{
+    static void Main()
+    {
+        Console.WriteLine("What you need to do: \n 1.Add Student details \n 2.delete student details \n 3.view the student details \n 4.update the student details");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("students details entered successfull");
+                break;
+            case 2:
+                Console.WriteLine("students details deleted successfull");
+                break;
+            case 3:
+                Console.WriteLine("students details viewed successfull");
+                break;
+            case 4:
+                Console.WriteLine("students details updated successfull");
+                break;
+            default:
+                Console.WriteLine("please select the corrct option");
+                break;
+        }
+
+    }
+}
